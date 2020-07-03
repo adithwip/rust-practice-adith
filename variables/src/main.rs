d9b12c8a3812cc7b7b75f0691ead4e8684f4b6ab
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-07-04 06:43:32 +0700

    func expression

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let x = 5;
-
-    let x = x + 1;
-
-    let x = x * 2;
+    let x = plus_one(5);
 
     println!("The value of x is: {}", x);
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
